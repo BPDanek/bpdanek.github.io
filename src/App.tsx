bf3778193b55cb2491d2eca84b5b97615093ffc8
BPDanek <bdanek@asu.edu>
2020-12-16 17:20:27 +0100

    github pages debugging

--- src/App.tsx
+++ src/App.tsx
@@ -2,4 +2,5 @@ import * as React from "react";
 import { render } from 'react-dom';
 import Page from "./pages/Page";
 
-render(<Page />, document.querySelector('#main'));
+// render(<Page />, document.querySelector('#main'));
+render(<h4>"hello</h4>, document.querySelector('#main'));
